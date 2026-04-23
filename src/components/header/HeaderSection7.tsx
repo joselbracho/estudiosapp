152dcf8f47d1344bd806e5ddf27f8db59c2f2906
Jose Bracho <josel.bracho@gmail.com>
2026-04-23 15:11:49 -0500

    changing contact data

--- src/components/header/HeaderSection7.tsx
+++ src/components/header/HeaderSection7.tsx
@@ -69,15 +69,12 @@ const HeaderSection7 = () => {
               <div className="rv-7-header-top__actions rv-14-header-top__actions">
                 <h6 className="rv-8-header-contact-info email">
                   <i className="fa-solid fa-envelope"></i>
-                  <a href="mailto:example@gmail.com">example@gmail.com</a>
+                  <a href="mailto:infoestudiosapp@gmail.com">infoestudiosapp@gmail.com</a>
                 </h6>
-                <h6 className="rv-8-header-contact-info location">
-                  <i className="fa-solid fa-sharp fa-location-dot"></i> New
-                  York, NY 10038
-                </h6>
+               
                 <h6 className="rv-8-header-contact-info numb">
                   <i className="fa-solid fa-sharp fa-phone"></i>{" "}
-                  <a href="tel:1237775643">(123) 777 - 5643</a>
+                  <a href="tel:+54 9 11 2713-5053">(+54) 9 11 2713-5053</a>
                 </h6>
               </div>
             </div>
