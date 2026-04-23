f3ad40179358fef0ad93fcd1fe215dd44086155a
Jose Bracho <josel.bracho@gmail.com>
2026-04-23 14:30:27 -0500

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export',      // Habilita la exportación estática
+  images: {
+    unoptimized: true,   // Obligatorio para GitHub Pages
+  },
+  basePath: '/estudiosapp', // Descomenta y pon el nombre de tu repo si no es un dominio personalizado
 };
 
 export default nextConfig;
